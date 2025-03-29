73e57a1cfbb0f2121a6180d1a17d4fc2ff60abde
gtmsingh <8234335+gtmsingh@users.noreply.github.com>
2025-03-29 14:51:51 +0530

    Added PING/PONG

--- app/main.py
+++ app/main.py
@@ -8,7 +8,9 @@ def main():
     # Uncomment this to pass the first stage
     #
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept()  # wait for client
+    connection, _ = server_socket.accept()  # wait for client
+
+    connection.sendAll(b"+PONG\r\n")  # send a PONG response
 
 
 if __name__ == "__main__":
